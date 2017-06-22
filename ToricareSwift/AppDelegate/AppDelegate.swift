a57776b7aac8128ce2ab5bf02aa3fe88d642fe46
MeoBeo <cryicestar92@gmail.com>
2017-06-22 11:24:51 +0700

    Init first xib view when run app

--- ToricareSwift/AppDelegate/AppDelegate.swift
+++ ToricareSwift/AppDelegate/AppDelegate.swift
@@ -16,6 +16,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        //first view when run app
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let mainVC = TRCLoginViewController(nibName: "TRCLoginViewController", bundle: nil)
+        window?.rootViewController = mainVC
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
