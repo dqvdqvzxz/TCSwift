3657bd5d0de9b88681f472edcf1add0c55221fa8
MeoBeo <cryicestar92@gmail.com>
2017-06-22 09:21:15 +0700

    First Commit

---
+++ ToricareSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ToricareSwift
+//
+//  Created by Mèo Béo on 6/22/17.
+//  Copyright © 2017 Mèo Béo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
