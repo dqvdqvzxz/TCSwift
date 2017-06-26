3082d856b3b796926b656e37ad952f2d27ebd763
MeoBeo <cryicestar92@gmail.com>
2017-06-26 08:20:55 +0700

    Adding file APIKeyContant

---
+++ ToricareSwift/Global/Contant/TRCAPIKeyContant.swift
@@ -0,0 +1,13 @@
+//
+//  TRCAPIKeyContant.swift
+//  ToricareSwift
+//
+//  Created by Mèo Béo on 6/26/17.
+//  Copyright © 2017 Mèo Béo. All rights reserved.
+//
+
+import UIKit
+
+//MARK: API
+let APP_DOMAIN = "http://172.16.210.126:80"
+let URL_LOGIN = "login"
--- ToricareSwift/Global/Contant/TRCStringConstant.swift
+++ ToricareSwift/Global/Contant/TRCStringConstant.swift
@@ -6,10 +6,6 @@
 //  Copyright © 2017 Mèo Béo. All rights reserved.
 //
 
-//MARK: API
-let APP_DOMAIN = "http://172.16.210.126:80"
-let URL_LOGIN = "login"
-
 //MARK: Title of View Controller
 let kTitleLogin = Localizable(value: "Login")
 
