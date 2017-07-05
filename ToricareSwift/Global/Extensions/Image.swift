06aa217e501bb467029933715ccbe2d12cc1df21
MeoBeo <cryicestar92@gmail.com>
2017-07-05 08:54:26 +0700

    Add extension UIImageView - make circle of image

---
+++ ToricareSwift/Global/Extensions/Image.swift
@@ -0,0 +1,17 @@
+//
+//  Image.swift
+//  ToricareSwift
+//
+//  Created by Mèo Béo on 7/5/17.
+//  Copyright © 2017 Mèo Béo. All rights reserved.
+//
+
+import UIKit
+
+extension UIImageView{
+    func makeCircle(){
+        let radius = self.frame.width / 2
+        self.layer.cornerRadius = radius
+        self.layer.masksToBounds = true
+    }
+}
