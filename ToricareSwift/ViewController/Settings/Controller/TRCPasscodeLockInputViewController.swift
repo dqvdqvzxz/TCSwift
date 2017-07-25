522428fa3293f6c33b4d8d848bba923360cf6967
MeoBeo <cryicestar92@gmail.com>
2017-07-25 16:32:29 +0700

    Progress length of text field and change image - PasscodeLockInputView

--- ToricareSwift/ViewController/Settings/Controller/TRCPasscodeLockInputViewController.swift
+++ ToricareSwift/ViewController/Settings/Controller/TRCPasscodeLockInputViewController.swift
@@ -10,10 +10,32 @@ import UIKit
 
 class TRCPasscodeLockInputViewController: TRCBaseViewController {
 
+    @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var lblError: UILabel!
+
+    @IBOutlet weak var viewMain: UIView!
+    
+    @IBOutlet weak var viewLeft: UIView!
+    @IBOutlet weak var viewImage1: UIView!
+    @IBOutlet weak var imgView1: UIImageView!
+    @IBOutlet weak var viewImage2: UIView!
+    @IBOutlet weak var imgView2: UIImageView!
+    
+    @IBOutlet weak var viewRight: UIView!
+    @IBOutlet weak var viewImage3: UIView!
+    @IBOutlet weak var imgView3: UIImageView!
+    @IBOutlet weak var viewImage4: UIView!
+    @IBOutlet weak var imgView4: UIImageView!
+    
+    @IBOutlet weak var tfPasscode: UITextField!
+    
+    var mode = String()
+    
+    //MARK: View controller
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        configUI()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +43,49 @@ class TRCPasscodeLockInputViewController: TRCBaseViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    //MARK: Config UI
+    func configUI(){
+        //navigation
+        self.navigationItem.title = ""
+        
+        //UI
+        imgView1.image = #imageLiteral(resourceName: "ic_passcode_line")
+        imgView2.image = #imageLiteral(resourceName: "ic_passcode_line")
+        imgView3.image = #imageLiteral(resourceName: "ic_passcode_line")
+        imgView4.image = #imageLiteral(resourceName: "ic_passcode_line")
+        
+        tfPasscode.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
     }
-    */
-
+    
+    //MARK: Action
+    func textFieldDidChange(_ textField: UITextField){
+        print(tfPasscode.text?.characters.count)
+        
+        if(tfPasscode.text?.characters.count == 0){
+            imgView1.image = #imageLiteral(resourceName: "ic_passcode_line")
+            imgView2.image = #imageLiteral(resourceName: "ic_passcode_line")
+            imgView3.image = #imageLiteral(resourceName: "ic_passcode_line")
+            imgView4.image = #imageLiteral(resourceName: "ic_passcode_line")
+        }else if(tfPasscode.text?.characters.count == 1){
+            imgView1.image = #imageLiteral(resourceName: "ic_passcode_circle")
+            imgView2.image = #imageLiteral(resourceName: "ic_passcode_line")
+            imgView3.image = #imageLiteral(resourceName: "ic_passcode_line")
+            imgView4.image = #imageLiteral(resourceName: "ic_passcode_line")
+        }else if(tfPasscode.text?.characters.count == 2){
+            imgView1.image = #imageLiteral(resourceName: "ic_passcode_circle")
+            imgView2.image = #imageLiteral(resourceName: "ic_passcode_circle")
+            imgView3.image = #imageLiteral(resourceName: "ic_passcode_line")
+            imgView4.image = #imageLiteral(resourceName: "ic_passcode_line")
+        }else if(tfPasscode.text?.characters.count == 3){
+            imgView1.image = #imageLiteral(resourceName: "ic_passcode_circle")
+            imgView2.image = #imageLiteral(resourceName: "ic_passcode_circle")
+            imgView3.image = #imageLiteral(resourceName: "ic_passcode_circle")
+            imgView4.image = #imageLiteral(resourceName: "ic_passcode_line")
+        }else if(tfPasscode.text?.characters.count == 4){
+            imgView1.image = #imageLiteral(resourceName: "ic_passcode_circle")
+            imgView2.image = #imageLiteral(resourceName: "ic_passcode_circle")
+            imgView3.image = #imageLiteral(resourceName: "ic_passcode_circle")
+            imgView4.image = #imageLiteral(resourceName: "ic_passcode_circle")
+        }
+    }
 }
