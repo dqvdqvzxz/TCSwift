435417b9043e8ca1612038582fa8c3b439d62f40
MeoBeo <cryicestar92@gmail.com>
2017-07-25 11:46:45 +0700

    Autolayout for PasscodeLockSettingView

--- ToricareSwift/ViewController/Settings/Controller/TRCPasscodeLockSettingViewController.swift
+++ ToricareSwift/ViewController/Settings/Controller/TRCPasscodeLockSettingViewController.swift
@@ -10,10 +10,15 @@ import UIKit
 
 class TRCPasscodeLockSettingViewController: TRCBaseViewController {
 
+    @IBOutlet weak var tblPasscode: UITableView!
+    
+    @IBOutlet weak var btnPasscode: UIButton!
+    
+    //View controller
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        configUI()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +26,48 @@ class TRCPasscodeLockSettingViewController: TRCBaseViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    //MARK: Config UI
+    func configUI(){
+        //navigation
+        
+        //table view
+        tblPasscode.dataSource = self
+        tblPasscode.delegate = self
+        tblPasscode.register(UINib(nibName: "TRCLinkedServiceCell", bundle: nil), forCellReuseIdentifier: "Cell")
+        
+        tblPasscode.tableFooterView = UIView()
+        tblPasscode.isScrollEnabled = false
     }
-    */
+    
+    //MARK: Button Action
+    @IBAction func tapBtnPasscode(_ sender: Any) {
+    }
+}
 
+extension TRCPasscodeLockSettingViewController: UITableViewDataSource{
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! TRCLinkedServiceCell
+        
+        cell.lblTime.isHidden = true
+        
+        return cell
+    }
+}
+
+extension TRCPasscodeLockSettingViewController: UITableViewDelegate{
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 44
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //
+    }
 }
