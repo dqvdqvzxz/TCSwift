9acb5cf0a5a3f4605c863c871466a3cf61ee441f
Mèo Béo <meobeo@Meo-Beo-2.local>
2017-08-01 11:02:49 +0700

    Change follow of MODE_MYPAGE in MyPageView

--- ToricareSwift/ViewController/MyPage/Controller/TRCMyPageViewController.swift
+++ ToricareSwift/ViewController/MyPage/Controller/TRCMyPageViewController.swift
@@ -119,7 +119,6 @@ extension TRCMyPageViewController: UICollectionViewDelegate{
             _obj.nc5.pushViewController(vc, animated: true)
         case 1:
             let vc = TRCMyPharmacyDetailPageViewController(nibName: "TRCMyPharmacyDetailPageViewController", bundle: nil)
-//            vc.mode = MODE_MYPAGE
             let backItem = UIBarButtonItem()
             backItem.title = STRING_BACK
             navigationItem.backBarButtonItem = backItem
