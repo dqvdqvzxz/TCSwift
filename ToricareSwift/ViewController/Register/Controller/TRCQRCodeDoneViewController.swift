916e8acfc2b32b2df0a0443337cd12c114c9e089
Mèo Béo <meobeo@Meo-Beo-2.local>
2017-08-14 11:44:31 +0700

    Call init tabbar in app

--- ToricareSwift/ViewController/Register/Controller/TRCQRCodeDoneViewController.swift
+++ ToricareSwift/ViewController/Register/Controller/TRCQRCodeDoneViewController.swift
@@ -62,6 +62,9 @@ class TRCQRCodeDoneViewController: TRCBaseViewController {
                 }
             }
         } else {
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            appDelegate.configTabbar()
+
             UIView.transition(with: self.view, duration: 0.5, options: .transitionFlipFromLeft, animations: {
                 UIApplication.shared.keyWindow?.rootViewController = _obj.tabController
             }, completion: { completed in
