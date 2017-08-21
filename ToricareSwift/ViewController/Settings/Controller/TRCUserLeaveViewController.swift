8897d7ea9be5e6d1a4cf3aa4d8cfb08ac403c5f8
Mèo Béo <meobeo@Meo-Beo-2.local>
2017-08-21 11:23:47 +0700

    Fix UserLeave

--- ToricareSwift/ViewController/Settings/Controller/TRCUserLeaveViewController.swift
+++ ToricareSwift/ViewController/Settings/Controller/TRCUserLeaveViewController.swift
@@ -71,27 +71,29 @@ class TRCUserLeaveViewController: TRCBaseViewController {
     
     //MARK: Button Action
     @IBAction func tapBtnLeave(_ sender: Any) {
-        TRCUserLeaveRequest().deleteAccount(UserDefaults.getUD(ACCESS_TOKEN) as! String, completion: { (data) in
-            let alert = UIAlertController(title: Localizable(value: "leave_confirm"),
-                                          message: Localizable(value: "really_want_leave_message"),
-                                          preferredStyle: UIAlertControllerStyle.alert)
-            
-            // add the action
-            alert.addAction(UIAlertAction(title: Localizable(value: "cancel"),
-                                          style: UIAlertActionStyle.cancel,
-                                          handler: nil))
-            alert.addAction(UIAlertAction(title: Localizable(value: "OK"),
-                                          style: UIAlertActionStyle.default,
-                                          handler: { action in
+        let alert = UIAlertController(title: Localizable(value: "leave_confirm"),
+                                      message: Localizable(value: "really_want_leave_message"),
+                                      preferredStyle: UIAlertControllerStyle.alert)
+        
+        // add the action
+        alert.addAction(UIAlertAction(title: Localizable(value: "cancel"),
+                                      style: UIAlertActionStyle.cancel,
+                                      handler: { action in
+                                        
+        }))
+        alert.addAction(UIAlertAction(title: Localizable(value: "OK"),
+                                      style: UIAlertActionStyle.default,
+                                      handler: { action in
+                                        TRCUserLeaveRequest().deleteAccount(UserDefaults.getUD(ACCESS_TOKEN) as! String, completion: { (data) in
                                             let vc = TRCUserLeaveCompleteViewController(nibName: "TRCUserLeaveCompleteViewController", bundle: nil)
                                             _obj.nc5.pushViewController(vc, animated: true)
-            }))
-            // show the alert
-            self.present(alert, animated: true, completion: nil)
-        }, failed: { (error) in
-            self.hideHUD()
-            self.showAlert(error)
-            ELog(error)
-        })
+                                        }, failed: { (error) in
+                                            self.hideHUD()
+                                            self.showAlert(error)
+                                            ELog(error)
+                                        })
+        }))
+        // show the alert
+        self.present(alert, animated: true, completion: nil)
     }
 }
