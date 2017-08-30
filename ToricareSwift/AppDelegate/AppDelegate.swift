564cb381a8112654b7b8912fb9fc4925133b81d0
Mèo Béo <meobeo@Meo-Beo-2.local>
2017-08-30 11:25:29 +0700

    Clear comment code in AppDelegate

--- ToricareSwift/AppDelegate/AppDelegate.swift
+++ ToricareSwift/AppDelegate/AppDelegate.swift
@@ -101,182 +101,31 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return handled
     }
     
-    
+    //MARK: Config UI
+    func configUI(){
 
-//    //MARK: Config UI
-//    func configUI(){
-//        
-//        //init window
-//        window = UIWindow(frame: UIScreen.main.bounds)
-//        
-//        
-//        //set theme style
-//        //        UITabBar.appearance().barTintColor = UIColor.init(hexString: WHITE_COLOR)
-//        
-//        UINavigationBar.appearance().barTintColor = UIColor.init(hexString: WHITE_COLOR)
-//        
-////        UINavigationBar.appearance().tintColor = UIColor.init(hexString: BUTTON_TITLE_COLOR)
-//        UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName: UIColor.init(hexString: MAIN_COLOR)]
-//        
-//        UITabBarItem.appearance().setTitleTextAttributes([NSForegroundColorAttributeName: UIColor.init(hexString: MAIN_COLOR)], for: .normal)
-//        UITabBarItem.appearance().setTitleTextAttributes([NSForegroundColorAttributeName: MAIN_COLOR], for: .highlighted)
-//        
-//        
-//        //init splash
-//        let mainVC = TRCSplashViewController(nibName: "TRCSplashViewController", bundle: nil)
-//        let navController = UINavigationController(rootViewController: mainVC)
-//        
-////        let mainVC = TRCSplashViewController(nibName: "View", bundle: nil)
-////        let navController = UINavigationController(rootViewController: mainVC)
-//
-//        
-//        //init root view
-//        window?.rootViewController = navController
-//        window?.makeKeyAndVisible()
-//    }
-//    
-//    func configTabbar(){
-//        //init root view controller
-//        let vc1 = TRCHomeViewController(nibName: "TRCHomeViewController", bundle: nil)
-//        let vc2 = TRCTab2ViewController(nibName: "TRCTab2ViewController", bundle: nil)
-//        let vc3 = TRCTab3ViewController(nibName: "TRCTab3ViewController", bundle: nil)
-//        let vc4 = TRCTab4ViewController(nibName: "TRCTab4ViewController", bundle: nil)
-//        let vc5 = TRCMyPageViewController(nibName: "TRCMyPageViewController", bundle: nil)
-//        
-//        //init root nav controller
-//        let nc1 = _obj.nc1
-//        let nc2 = _obj.nc2
-//        let nc3 = _obj.nc3
-//        let nc4 = _obj.nc4
-//        let nc5 = _obj.nc5
-//        
-//        //init tabbar
-//        nc1.viewControllers = [vc1]
-//        nc2.viewControllers = [vc2]
-//        nc3.viewControllers = [vc3]
-//        nc4.viewControllers = [vc4]
-//        nc5.viewControllers = [vc5]
-//        _obj.tabController.viewControllers = [nc1, nc2, nc3, nc4, nc5]
-//        
-//        
-//        //set default tab
-//        _obj.tabController.selectedIndex = 0
-//        
-//        //set item for tabbar item
-//        let item1 = _obj.tabController.tabBar.items?[0]
-//        let item2 = _obj.tabController.tabBar.items?[1]
-//        let item3 = _obj.tabController.tabBar.items?[2]
-//        let item4 = _obj.tabController.tabBar.items?[3]
-//        let item5 = _obj.tabController.tabBar.items?[4]
-//        
-//        //set title for tab item
-//        item1?.title = STRING_TAB_HOME
-//        item2?.title = STRING_TAB_NUTRITION
-//        item3?.title = STRING_TAB_EXERCISE
-//        item4?.title = STRING_TAB_MEDICINE
-//        item5?.title = STRING_TAB_MYPAGE
-//        
-//        item1?.image = #imageLiteral(resourceName: "home")
-//        item1?.selectedImage = #imageLiteral(resourceName: "home_hl")
-//        
-//        item2?.image = #imageLiteral(resourceName: "nutrition")
-//        item2?.selectedImage = #imageLiteral(resourceName: "nutrition_hl")
-//        
-//        item3?.image = #imageLiteral(resourceName: "run")
-//        item3?.selectedImage = #imageLiteral(resourceName: "run_hl")
-//        
-//        item4?.image = #imageLiteral(resourceName: "pill")
-//        item4?.selectedImage = #imageLiteral(resourceName: "pill_hl")
-//        
-//        item5?.image = #imageLiteral(resourceName: "profile")
-//        item5?.selectedImage = #imageLiteral(resourceName: "profile_hl")
-//        
-//        let selectedItem = [NSForegroundColorAttributeName: UIColor.init(hexString: MAIN_COLOR)]
-//        let uhnSelectedItem = [NSForegroundColorAttributeName: UIColor.init(hexString: LABEL_FONT_GREY_COLOR)]
-//        
-//        item1?.setTitleTextAttributes(uhnSelectedItem, for: .normal)
-//        item1?.setTitleTextAttributes(selectedItem, for: .selected)
-//        
-//        item2?.setTitleTextAttributes(uhnSelectedItem, for: .normal)
-//        item2?.setTitleTextAttributes(selectedItem, for: .selected)
-//        
-//        item3?.setTitleTextAttributes(uhnSelectedItem, for: .normal)
-//        item3?.setTitleTextAttributes(selectedItem, for: .selected)
-//        
-//        item4?.setTitleTextAttributes(uhnSelectedItem, for: .normal)
-//        item4?.setTitleTextAttributes(selectedItem, for: .selected)
-//        
-//        item5?.setTitleTextAttributes(uhnSelectedItem, for: .normal)
-//        item5?.setTitleTextAttributes(selectedItem, for: .selected)
-//    }
-    
-    
-        //MARK: Config UI
-        func configUI(){
-    
-            //init window
-            window = UIWindow(frame: UIScreen.main.bounds)
-            
-            //set color for navigation
-            UINavigationBar.appearance().barTintColor = UIColor.init(hexString: WHITE_COLOR)
-            UINavigationBar.appearance().tintColor = UIColor.init(hexString: MAIN_COLOR)
-            UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName: UIColor.init(hexString: MAIN_COLOR)]
-            
-            //set color for tabbar
-            UITabBar.appearance() .isTranslucent = false
-            UITabBar.appearance().barTintColor = UIColor.init(hexString: WHITE_COLOR)
-            UITabBar.appearance().tintColor = UIColor.init(hexString: WHITE_COLOR)
-            
-            _obj.tabController.delegate = self
-            
-            //init root view
-            let mainVC = TRCSplashViewController(nibName: "TRCSplashViewController", bundle: nil)
-            let navController = UINavigationController(rootViewController: mainVC)
-            
-            window?.rootViewController = navController
-            window?.makeKeyAndVisible()
-        }
-    
-    
-//    func configTabbar(){
-//        //set background color for selected item
-//        _obj.tabController.tabBar.selectionIndicatorImage = UIImage().makeImageWithColorAndSize(color: UIColor.init(hexString: TABBAR_COLOR_1), size: CGSize(width: _obj.tabController.tabBar.frame.width/5, height: _obj.tabController.tabBar.frame.height))
-//        
-//        //setcolor for text
-//        let selectedItem = [NSForegroundColorAttributeName: UIColor.init(hexString: WHITE_COLOR)]
-//        let unSelectedItem = [NSForegroundColorAttributeName: UIColor.init(hexString: TABBAR_COLOR_1)]
-//        
-//        _obj.tabController.tabBar.items?[0].setTitleTextAttributes(unSelectedItem, for: .normal)
-//        _obj.tabController.tabBar.items?[0].setTitleTextAttributes(selectedItem, for: .selected)
-//        
-//        _obj.tabController.tabBar.items?[1].setTitleTextAttributes(unSelectedItem, for: .normal)
-//        _obj.tabController.tabBar.items?[1].setTitleTextAttributes(selectedItem, for: .selected)
-//        
-//        _obj.tabController.tabBar.items?[2].setTitleTextAttributes(unSelectedItem, for: .normal)
-//        _obj.tabController.tabBar.items?[2].setTitleTextAttributes(selectedItem, for: .selected)
-//        
-//        _obj.tabController.tabBar.items?[3].setTitleTextAttributes(unSelectedItem, for: .normal)
-//        _obj.tabController.tabBar.items?[3].setTitleTextAttributes(selectedItem, for: .selected)
-//        
-//        _obj.tabController.tabBar.items?[4].setTitleTextAttributes(unSelectedItem, for: .normal)
-//        _obj.tabController.tabBar.items?[4].setTitleTextAttributes(selectedItem, for: .selected)
-//        
-//        //set image
-//        _obj.tabController.tabBar.items?[0].image = #imageLiteral(resourceName: "menu_home_00").withRenderingMode(.alwaysOriginal)
-//        _obj.tabController.tabBar.items?[0].selectedImage = #imageLiteral(resourceName: "menu_home_w").withRenderingMode(.alwaysOriginal)
-//        
-//        _obj.tabController.tabBar.items?[1].image = #imageLiteral(resourceName: "menu_syokuji_00").withRenderingMode(.alwaysOriginal)
-//        _obj.tabController.tabBar.items?[1].selectedImage = #imageLiteral(resourceName: "menu_syokuji_w").withRenderingMode(.alwaysOriginal)
-//        
-//        _obj.tabController.tabBar.items?[2].image = #imageLiteral(resourceName: "menu_katsudou_00").withRenderingMode(.alwaysOriginal)
-//        _obj.tabController.tabBar.items?[2].selectedImage = #imageLiteral(resourceName: "menu_katsudou_w").withRenderingMode(.alwaysOriginal)
-//        
-//        _obj.tabController.tabBar.items?[3].image = #imageLiteral(resourceName: "menu_okusuri_00").withRenderingMode(.alwaysOriginal)
-//        _obj.tabController.tabBar.items?[3].selectedImage = #imageLiteral(resourceName: "menu_okusuri_w").withRenderingMode(.alwaysOriginal)
-//        
-//        _obj.tabController.tabBar.items?[4].image = #imageLiteral(resourceName: "menu_mypage_00").withRenderingMode(.alwaysOriginal)
-//        _obj.tabController.tabBar.items?[4].selectedImage = #imageLiteral(resourceName: "menu_mypage_w").withRenderingMode(.alwaysOriginal)
-//    }
+        //init window
+        window = UIWindow(frame: UIScreen.main.bounds)
+        
+        //set color for navigation
+        UINavigationBar.appearance().barTintColor = UIColor.init(hexString: WHITE_COLOR)
+        UINavigationBar.appearance().tintColor = UIColor.init(hexString: MAIN_COLOR)
+        UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName: UIColor.init(hexString: MAIN_COLOR)]
+        
+        //set color for tabbar
+        UITabBar.appearance() .isTranslucent = false
+        UITabBar.appearance().barTintColor = UIColor.init(hexString: WHITE_COLOR)
+        UITabBar.appearance().tintColor = UIColor.init(hexString: WHITE_COLOR)
+        
+        _obj.tabController.delegate = self
+        
+        //init root view
+        let mainVC = TRCSplashViewController(nibName: "TRCSplashViewController", bundle: nil)
+        let navController = UINavigationController(rootViewController: mainVC)
+        
+        window?.rootViewController = navController
+        window?.makeKeyAndVisible()
+    }
 }
 
 extension AppDelegate: UITabBarControllerDelegate{
