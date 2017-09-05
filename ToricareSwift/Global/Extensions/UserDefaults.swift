7891dad22394a85ec395c8e9947cc8fb6f91b352
tuanla6242 <tuanla6242@co-well.com.vn>
2017-09-05 16:11:15 +0700

    #46534 | Fix bug passcode lock

--- ToricareSwift/Global/Extensions/UserDefaults.swift
+++ ToricareSwift/Global/Extensions/UserDefaults.swift
@@ -25,7 +25,7 @@ extension UserDefaults{
     
     class func resetCommonValue() {
         for key in Array(UserDefaults.standard.dictionaryRepresentation().keys) {
-            if (key != ACCESS_TOKEN && key != FB_TOKEN && key != REFRESH_ACCESS_TOKEN) {
+            if (key != ACCESS_TOKEN && key != FB_TOKEN && key != REFRESH_ACCESS_TOKEN && key != PASSCODE) {
                 UserDefaults.standard.removeObject(forKey: key)
             }
         }
